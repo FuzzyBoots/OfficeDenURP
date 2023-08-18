e71d2a55081c2c40cde35645efe64d2a2757fa02
Sean Duggan <Sean.Duggan@gmail.com>
2023-08-18 10:57:23 -0400

    Fiddled a bit with things while writing the article, added a Delect All script

---
+++ Assets/Editor/DeselectAll.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+
+public static class DeselectAll
+{
+    [MenuItem("Edit/Deselect All &d", false, -101)]
+    static void Deselect()
+    {
+        Selection.activeGameObject = null;
+    }
+}
